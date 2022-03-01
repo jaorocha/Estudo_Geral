e09dbc8909bcb0740560d9f6fceae26ae9506bf4
joaorocha <joaorocha.net@gmail.com>
2022-03-01 11:56:49 -0300

    estudo javaScript

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+// Meu primeiro código - comentários
+
+/*
+comenta bloco
+*/
+
+alert("Olá pessoal!");
+
+
+// variaveis
+
+var mensagem ="Olá pessoal 2!";
+alert(mensagem);
+
+//operações matematicas
+
+var a = 2;
+var b = 3;
+var c = "teste";
+
+var soma = a+b;
+var sub = a-b;
+var mult = a*b;
+var div = a/b;
+
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
+
+//estrutura condicional
+
+if (c%2 ==1){
+    alert("Numero Impar");
+}
+else if (c%2 ==0) {
+    alert("Numero par");
+}
+else {
+    alert("Valor inválido");
+}
+
